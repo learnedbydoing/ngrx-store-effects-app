9bc4f67bd8a7e744a34bf0ecb3913834bf0fe8d6
Learned By Doing <learnedbydoing@gmail.com>
2018-12-09 14:43:53 +0100

    Optimizing Data Structures with Entities

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -4,15 +4,35 @@ import * as fromPizzas from "../actions/pizza.action";
 import {Pizza} from "../../models/pizza.model";
 import {PizzaAction} from "../actions/pizza.action";
 
-export interface PizzaState {
-  //defining a slice of state that reducer will manage in the entire state tree
-  data: Pizza[];
+export interface PizzaState { //defining a slice of state that reducer will manage in the entire state tree
+  /*  data: Pizza[]; //does not scale, it is better to work with object (aka entities), similar to backend
+  *   think of NgRx store as a kind of database for the client
+  * */
+  entities: { [id: number]: Pizza } //object with entities, each entity identified by ID + will hold a pizza
   loaded: boolean;
   loading: boolean;
 }
 
+/*
+*   from action.payload we get this structure [{ id: 1}, { id:2 }] (=array of objects)
+*   we need to convert this structure into entities
+*   example entities object:
+*
+    const pizza[any] = {
+      1: {
+        id: 1,
+        name: 'pizza salami',
+        toppings: []
+      },
+      2: {}
+    }
+
+* */
+
+
+
 export const initialState: PizzaState = {
-  data: [],
+  entities: {},
   loaded: false,
   loading: false
 };
@@ -33,12 +53,23 @@ export function reducer(
     }
 
     case fromPizzas.LOAD_PIZZAS_SUCCESS: {
-      const data = action.payload; //get data provided
+      const pizzas = action.payload; //get data provided
+
+      const entities = pizzas.reduce(
+        (entities: { [id: number]: Pizza }, pizza: Pizza) => {
+          return {
+            ...entities,
+            [pizza.id]: pizza
+          }
+        }, {
+          ...state.entities //initial value for reducer function
+        })
+
       return {
         ...state,
         loading: false,
         loaded: true,
-        data //merge with current state
+        entities
       }
     }
 
@@ -56,6 +87,6 @@ export function reducer(
 
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
-export const getPizzas = (state: PizzaState) => state.data;
+export const getPizzasEntities = (state: PizzaState) => state.entities;
 //..these are small functions that get passed a small level of the PizzaState and at that point in time we are down in our data structures
 // => good practice to put such functions under our reducer
